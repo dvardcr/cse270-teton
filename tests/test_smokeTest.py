e78dc7943719c0ee3c810d909ed80e15277ce936
Diego Vargas <diego.vargasd@gmail.com>
2025-08-06 10:03:52 -0600

    Updated Selenium Smoke Test

--- tests/test_smokeTest.py
+++ tests/test_smokeTest.py
@@ -9,10 +9,13 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
 
 class TestSmokeTest():
   def setup_method(self, method):
-    self.driver = webdriver.Firefox()
+    options = Options()
+    options.add_argument("--headless=new")
+    self.driver = webdriver.Chrome(options=options)
     self.vars = {}
   
   def teardown_method(self, method):
@@ -65,8 +68,7 @@ class TestSmokeTest():
     self.driver.find_element(By.NAME, "bizname").send_keys("New Bizness")
     self.driver.find_element(By.NAME, "biztitle").send_keys("Manager")
     self.driver.find_element(By.NAME, "submit").click()
-    elements = self.driver.find_elements(By.NAME, "email")
-    assert len(elements) > 0
+    WebDriverWait(self.driver, 30).until(expected_conditions.presence_of_element_located((By.NAME, "email")))
   
   def test_5Navigatetotheadminpage(self):
     self.driver.get("http://127.0.0.1:5500/teton/1.6/admin.html")
@@ -76,5 +78,5 @@ class TestSmokeTest():
     self.driver.find_element(By.ID, "username").send_keys("dvd")
     self.driver.find_element(By.ID, "password").send_keys("123456789")
     self.driver.find_element(By.CSS_SELECTOR, ".mysubmit:nth-child(4)").click()
-    assert self.driver.find_element(By.CSS_SELECTOR, ".errorMessage").text == "Invalid username and password."
+    WebDriverWait(self.driver, 30).until(expected_conditions.text_to_be_present_in_element((By.CSS_SELECTOR, ".errorMessage"), "Invalid username and password."))
   
